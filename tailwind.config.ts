fad4440e24dd2532fcffc715094c1c2dfeddbbe5
ashik-ad <dev.ashikdhimal@gmail.com>
2023-12-11 13:47:08 +0545

    chore(config): custom gradient color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-gray': 'linear-gradient(113deg, rgba(255, 255, 255, 0.37) 10.29%, rgba(255, 255, 255, 0.00) 100.35%)'
       },
       colors: {
         skin: '#2BB32A'

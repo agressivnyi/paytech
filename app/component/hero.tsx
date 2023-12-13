de79f0838ef4f08c5861608cb2b49685b0ae9cdc
ashik-ad <dev.ashikdhimal@gmail.com>
2023-12-13 08:59:40 +0545

    chore(layout & responsive): remove margin bottom of main & responsive header title and card

--- app/component/hero.tsx
+++ app/component/hero.tsx
@@ -5,10 +5,10 @@ export default function Hero() {
   return (
     <header className="grid grid-cols-1 gap-y-24 md:grid-cols-2">
       <div className="flex flex-col gap-8">
-        <h1 className="text-4xl font-bold">
+        <h1 className="text-4xl font-bold lg:text-6xl">
           Discover the Perfect Credit Card for You
         </h1>
-        <p className="text-base text-[#ADB2B1] md:max-w-[90%]">
+        <p className="text-base text-[#ADB2B1] md:max-w-[70%]">
           Discover the power of our secure and rewarding credit cards. Explore
           our range of credit cards and take control of your finances today.
         </p>
@@ -25,9 +25,9 @@ export default function Hero() {
               <path
                 d="M1 9.75H23.5M23.5 9.75L16.5 18.25M23.5 9.75L16.5 1.75"
                 stroke="#EFF8F0"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
             </svg>
           }
@@ -36,7 +36,13 @@ export default function Hero() {
         </Button>
       </div>
       <div className="relative h-fill flex justify-end">
-        <CreditCard className="absolute min-w-[280px] left-0 md:min-w-56 lg:w-[550px] bg-gradient-gray rotate-6 backdrop-blur-2xl z-10" />
+        <CreditCard
+          className="absolute min-w-[320px] top-7 right-0 text-sm border-gray-50 md:min-w-56 
+                  lg:text-base lg:w-[520px] bg-gradient-gray rotate-6 backdrop-blur-[37px] z-10"
+          logoClass="w-8 md:w-auto"
+          signalClass="w-8 h-8 md:w-auto md:h-auto"
+          chipClass="w-8 md:w-auto"
+        />
         <div className="relative h-72 w-72 bg-skin bottom-0 -right-[40%] lg:w-[492px] lg:h-[492px] md:-right-[33%] rounded-full z-0" />
       </div>
     </header>
